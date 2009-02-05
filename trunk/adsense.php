5f974334ea7115c3cbd4d0784816b740c72723e8
vklepikovskiy <vklepikovskiy@9ae58a30-f2de-11dd-89c5-9df526a60542>
2009-02-05 13:10:58 +0000

    Added license and copyrights

--- trunk/adsense.php
+++ trunk/adsense.php
@@ -1,11 +1,27 @@
 <?php
 
-/**
- * PHP class ment to collect AdSense account data
- * It can return various data for different periods of time
+/*
+ * PHP class that can retrieve data from AdSense account
+ * It's based on PHP AdSense account monitor class (http://www.webtoolkit.info/php-adsense-account-monitor.html)
+ * Copyright (C) 2009  Alex Polski
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
- * @copyright    Copyright © 2007
- * @package        AdSense
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/**
+ * PHP class that can retrieve data from AdSense account
+ * @package AdSense
  */
 class AdSense {
 
