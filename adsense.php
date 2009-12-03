cefff25a4f0a86fed65dbbaaff9dff867926d184
alexander.makarow <alexander.makarow@9ae58a30-f2de-11dd-89c5-9df526a60542>
2009-12-03 00:15:14 +0000

    Changed eregi to preg_match since ereg is deprecated in PHP 5.3, some code optimizations.

--- adsense.php
+++ adsense.php
@@ -1,9 +1,10 @@
 <?php
-
-/*
- * PHP class that can retrieve data from AdSense account
+/**
+ * PHP class that can retrieve data from AdSense account.
+ * 
  * It's based on PHP AdSense account monitor class (http://www.webtoolkit.info/php-adsense-account-monitor.html)
- * Copyright (C) 2009  Alex Polski
+ * Copyright © 2009, Alex Polski
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,29 +12,28 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 /**
  * sys_get_temp_dir function for the proper work in PHP4
  * Based on http://www.php.net/sys_get_temp_dir
  */
 if (!function_exists('sys_get_temp_dir')) {
-  function sys_get_temp_dir() {
-    if (!empty($_ENV['TMP'])) { return realpath($_ENV['TMP']); }
-    if (!empty($_ENV['TMPDIR'])) { return realpath( $_ENV['TMPDIR']); }
-    if (!empty($_ENV['TEMP'])) { return realpath( $_ENV['TEMP']); }
-    $tempfile = tempnam(uniqid(rand(), TRUE), '');
-    if (file_exists($tempfile)) {
-      unlink($tempfile);
-      return realpath(dirname($tempfile));
+    function sys_get_temp_dir() {
+        if(!empty($_ENV['TMP'])) return realpath($_ENV['TMP']);
+        if(!empty($_ENV['TMPDIR'])) return realpath($_ENV['TMPDIR']);
+        if(!empty($_ENV['TEMP'])) return realpath($_ENV['TEMP']);
+        $tempfile = tempnam(uniqid(rand(), TRUE), '');
+        if(file_exists($tempfile)){
+            unlink($tempfile);
+            return realpath(dirname($tempfile));
+        }
     }
-  }
 }
 
 /**
@@ -46,7 +46,6 @@ class AdSense {
      * @var resource
      */
     var $curl = null;
-
 
     /**
      * AdSense::AdSense()
@@ -68,7 +67,6 @@ class AdSense {
         register_shutdown_function(array(&$this, '__destructor'));
     }
 
-
     /**
      * AdSense::__destructor()
      * AdSense class destructor
@@ -78,11 +76,10 @@ class AdSense {
         @unlink($this->cookieFile);
     }
 
-
     /**
      * AdSense::connect()
      * Connects to AdSense account using supplied credentials
-     * Returns true on unsuccessful connection, false otherwise
+     * Returns true on successful connection, false otherwise
      *
      * @param string $username AdSense username
      * @param string $password AdSense password
@@ -99,9 +96,10 @@ class AdSense {
         curl_setopt($this->curl, CURLOPT_URL, 'https://www.google.com/adsense/login-box.js');
         $content = curl_exec($this->curl);
         $content = preg_replace(
-          array("/\\\\75/", "/\\\\42/", "/\\\\46/", "/\\\\075/"),
-          array('=', '"', '&', '='),
-          $content);
+            array("/\\\\75/", "/\\\\42/", "/\\\\46/", "/\\\\075/"),
+            array('=', '"', '&', '='),
+            $content
+        );
         preg_match('/src="([^"]+)"/', $content, $match);
         $next_url = $match[1];
         $next_url = str_replace('&amp;', '&', $next_url);
@@ -112,19 +110,19 @@ class AdSense {
         $content = curl_exec($this->curl);
         preg_match_all('<input type="hidden" name="(.*?)" value="(.*?)">', curl_exec($this->curl), $out);
         $params = array();
-        foreach($out[1] as $key=>$name) {
-          $params[] = $name . '=' . urlencode($out[2][$key]);
+        foreach($out[1] as $key => $name) {
+            $params[] = $name.'='.urlencode($out[2][$key]);
         }
-        $params[] = 'Email=' . urlencode($username);
-        $params[] = 'Passwd=' . urlencode($password);
-        $params[] = 'null=' . urlencode('Sign in');
+        $params[] = 'Email='.urlencode($username);
+        $params[] = 'Passwd='.urlencode($password);
+        $params[] = 'null='.urlencode('Sign in');
 
         // /accounts/ServiceLoginBoxAuth
         curl_setopt($this->curl, CURLOPT_POST, true);
         curl_setopt($this->curl, CURLOPT_URL, "https://www.google.com/accounts/ServiceLoginBoxAuth");
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, join('&', $params));
         $content = curl_exec($this->curl);
-        preg_match("/<a href=\"([^\"]+)\"[^>]*>click here to continue<\/a>/", $content, $match);
+        preg_match('~<a href="([^"]+)"[^>]*>click here to continue</a>~', $content, $match);
         $next_url = $match[1];
         $next_url = str_replace('&amp;', '&', $next_url);
 
@@ -142,11 +140,7 @@ class AdSense {
         $content = curl_exec($this->curl);
 
         // did we login ?
-        if (eregi("<a href=\"/adsense/signout\">",  $content)) {
-            return true;
-        } else {
-            return false;
-        }
+        return preg_match('~<a href="/adsense/signout">~i',  $content);
     }
 
     /**
@@ -166,7 +160,7 @@ class AdSense {
      * @return array
      */
     function parse($content){
-        preg_match_all('/<td nowrap valign="top" style="text-align:right" class="">(.*?)<\/td>/', $content, $matches);
+        preg_match_all('~<td nowrap valign="top" style="text-align:right" class="">(.*?)</td>~', $content, $matches);
         return array(
             "impressions" => $matches[1][0],
             "clicks" => $matches[1][1],
@@ -174,9 +168,7 @@ class AdSense {
             "ecpm" => $matches[1][3],
             "earnings" => $matches[1][4]
         );
-
     }
-
 
     /**
      * AdSense::today()
@@ -190,7 +182,6 @@ class AdSense {
         return $this->parse(curl_exec($this->curl));
     }
 
-
     /**
      * AdSense::yesterday()
      * Gets AdSense data for the period: yesterday
@@ -202,7 +193,6 @@ class AdSense {
         curl_setopt($this->curl, CURLOPT_URL, "https://www.google.com/adsense/report/overview?timePeriod=yesterday");
         return $this->parse(curl_exec($this->curl));
     }
-
 
     /**
      * AdSense::last7days()
@@ -216,7 +206,6 @@ class AdSense {
         return $this->parse(curl_exec($this->curl));
     }
 
-
     /**
      * AdSense::lastmonth()
      * Gets AdSense data for the period: lastmonth
@@ -229,7 +218,6 @@ class AdSense {
         return $this->parse(curl_exec($this->curl));
     }
 
-
     /**
      * AdSense::thismonth()
      * Gets AdSense data for the period: thismonth
@@ -241,7 +229,6 @@ class AdSense {
         curl_setopt($this->curl, CURLOPT_URL, "https://www.google.com/adsense/report/overview?timePeriod=thismonth");
         return $this->parse(curl_exec($this->curl));
     }
-
 
     /**
      * AdSense::sincelastpayment()
@@ -276,6 +263,7 @@ class AdSense {
     /**
      * Get csv data from custom report
 	 * Warning: if you will edit report at Google Adsense page, its id will change!
+     * Use get_report_id_from_name to get id by report name.
      *
      * @param int $id
      * @param string $encoding
@@ -296,103 +284,121 @@ class AdSense {
 	 * @return array
 	 */
     function report($report_id){
-      $result = array();
-      curl_setopt($this->curl, CURLOPT_URL, "https://www.google.com/adsense/report/view-custom.do?reportId=$report_id");
-      $content = curl_exec($this->curl);
-      if (preg_match('/var\s+reportTable\s+=\s+new\s+AsyncReportTable\(([^\)]+)\);/si', $content, $matches)) {
-        $params = array();
-        foreach (explode(",\n", $matches[1]) as $v) {
-          $params[] = trim($v, " \t'\"\n\r");
-        }
-        curl_setopt($this->curl, CURLOPT_URL,
-          'https://www.google.com/adsense/report/online-stored-reporttable?storedReportId=' .
-          $params[0] . '&reportUri=' . str_replace('?', '%3F', str_replace('/', '%2F', str_replace('\x', '%', $params[2]))) .
-          '&formId=' . $params[3] . '&title=' . str_replace('+', '%20', urlencode($params[1])) . '&waitTime0');
-        do {
-          $content = curl_exec($this->curl);
-          if (empty($content)) {
-            sleep(1);
-          }
-        } while (empty($content));
-        if (preg_match_all('/<tr\s+class="(odd|even)\s+datarow">\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<\/tr>/si', $content, $matches)) {
-            foreach ($matches[2] as $k => $v) {
-              $result[$k]['channel'] = preg_replace('/<.+>/', '', $v);
-              $result[$k]['impressions'] = $matches[3][$k];
-              $result[$k]['clicks'] = $matches[4][$k];
-              $result[$k]['ctr'] = $matches[5][$k];
-              $result[$k]['ecpm'] = $matches[6][$k];
-              $result[$k]['earnings'] = $matches[7][$k];
+        $result = array();
+        curl_setopt($this->curl, CURLOPT_URL, "https://www.google.com/adsense/report/view-custom.do?reportId=$report_id");
+        $content = curl_exec($this->curl);
+        if (preg_match('/var\s+reportTable\s+=\s+new\s+AsyncReportTable\(([^\)]+)\);/si', $content, $matches)) {
+            $params = array();
+            foreach (explode(",\n", $matches[1]) as $v) {
+                $params[] = trim($v, " \t'\"\n\r");
+            }
+            curl_setopt($this->curl, CURLOPT_URL,
+                'https://www.google.com/adsense/report/online-stored-reporttable?storedReportId='.$params[0]
+                 .'&reportUri='.str_replace('?', '%3F', str_replace('/', '%2F', str_replace('\x', '%', $params[2])))
+                 .'&formId='.$params[3]
+                 .'&title='.str_replace('+', '%20', urlencode($params[1])) . '&waitTime0'
+            );
+            do {
+                $content = curl_exec($this->curl);
+                if (empty($content)) sleep(1);
+            }
+            while (empty($content));
+
+            if (preg_match_all('/<tr\s+class="(odd|even)\s+datarow">\s*' .
+                '<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<\/tr>/si', $content, $matches
+            )){
+                foreach ($matches[2] as $k => $v) {
+                    $result[$k] = array(
+                        'channel' => preg_replace('/<.+>/', '', $v),
+                        'impressions' => $matches[3][$k],
+                        'clicks' => $matches[4][$k],
+                        'ctr' => $matches[5][$k],
+                        'ecpm' => $matches[6][$k],
+                        'earnings' => $matches[7][$k]
+                    );
+                }
             }
         }
-      }
-      return $result;
+        return $result;
     }
 
     function quick_report($url){
-      $result = array();
-      curl_setopt($this->curl, CURLOPT_URL, $url);
-      $content = curl_exec($this->curl);
-      if (preg_match('/var\s+reportTable\s+=\s+new\s+AsyncReportTable\(([^\)]+)\);/si', $content, $matches)) {
-        $params = array();
-        foreach (explode(",\n", $matches[1]) as $v) {
-          $params[] = trim($v, " \t'\"\n\r");
-        }
-        $reportUri = preg_replace(
-          array('/&/', '/=/', '/\?/', '/\//'),
-          array('%26', '%3D', '%3F', '%2F'),
-          $params[2]);
-        $reportUri = str_replace('\x', '%', $reportUri);
-        curl_setopt($this->curl, CURLOPT_URL,
-          'https://www.google.com/adsense/report/online-stored-reporttable?storedReportId=' .
-          $params[0] . '&reportUri=' . $reportUri .
-          '&formId=' . $params[3] . '&title=' . str_replace('+', '%20', urlencode($params[1])) . '&waitTime0');
-        $tr = 0;
-        do {
-          $content = curl_exec($this->curl);
-          if (empty($content) or $content === true) {
-            sleep(1);
-            if (++$tr > 10) {
-              echo "Can't get the report data!\n";
-              die;
+        $result = array();
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        $content = curl_exec($this->curl);
+        if (preg_match('/var\s+reportTable\s+=\s+new\s+AsyncReportTable\(([^\)]+)\);/si', $content, $matches)) {
+            $params = array();
+            foreach (explode(",\n", $matches[1]) as $v) {
+                $params[] = trim($v, " \t'\"\n\r");
             }
-          }
-        } while (empty($content) or $content === true);
-        if (preg_match_all('/<tr\s+class="(odd|even)\s+datarow">\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<td[^>]*>(.*?)<\/td>\s*' .
-          '<\/tr>/si', $content, $matches)) {
-            $day = 0;
-            $n = 0;
-            foreach ($matches[3] as $k => $v) {
-              if ($day === 0) {
-                $day = date('Y-m-d', strtotime($matches[2][$k]));
-              }
-              if (isset($matches[2][$k - 1]) && $matches[2][$k - 1] != $matches[2][$k]) {
-                $day = date('Y-m-d', strtotime($matches[2][$k]));
+            $reportUri = strtr($params[2], array(
+                '&' => '%26',
+                '=' => '%3D',
+                '?' => '%3F',
+                '/' => '%2F'
+            ));
+            $reportUri = str_replace('\x', '%', $reportUri);
+            curl_setopt($this->curl, CURLOPT_URL,
+                'https://www.google.com/adsense/report/online-stored-reporttable?storedReportId='.$params[0]
+                .'&reportUri='.$reportUri
+                .'&formId='.$params[3]
+                .'&title='.str_replace('+', '%20', urlencode($params[1]))
+                .'&waitTime0'
+            );
+            $tr = 0;
+            do {
+                $content = curl_exec($this->curl);
+                if (empty($content) || $content === true) {
+                    sleep(1);
+                    if (++$tr > 10) {
+                        echo "Can't get the report data!\n";
+                        die;
+                    }
+                }
+            }
+            while (empty($content) || $content === true);
+
+            if (preg_match_all(
+                '~<tr\s+class="(odd|even)\s+datarow">\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'<td[^>]*>(.*?)<\/td>\s*'
+                .'</tr>~si', $content, $matches
+            )){
+                $day = 0;
                 $n = 0;
-              }
-              $result[$day][$n]['channel'] = preg_replace('/\s*<[^>]+>\s*/', '', $v);
-              $result[$day][$n]['impressions'] = $matches[4][$k];
-              $result[$day][$n]['clicks'] = $matches[5][$k];
-              $result[$day][$n]['ctr'] = $matches[6][$k];
-              $result[$day][$n]['ecpm'] = $matches[7][$k];
-              $result[$day][$n]['earnings'] = $matches[8][$k];
-              $n++;
+                foreach ($matches[3] as $k => $v) {
+                    if ($day === 0) {
+                        $day = date('Y-m-d', strtotime($matches[2][$k]));
+                    }
+                    if (isset($matches[2][$k - 1]) && $matches[2][$k - 1] != $matches[2][$k]) {
+                        $day = date('Y-m-d', strtotime($matches[2][$k]));
+                        $n = 0;
+                    }
+
+                    $result[$day][$n] = array(
+                        'channel' => preg_replace('/\s*<[^>]+>\s*/', '', $v),
+                        'impressions' => $matches[4][$k],
+                        'clicks' => $matches[5][$k],
+                        'ctr' => $matches[6][$k],
+                        'ecpm' => $matches[7][$k],
+                        'earnings' => $matches[8][$k]
+                    );
+
+                    $n++;
+                }
             }
         }
-      }
-      return $result;
+        return $result;
     }
 }
